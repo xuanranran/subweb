b486e14e7ac124fb150fe8a023691667b30e11a2
LovinYarn <juanshengyuan@gmail.com>
2024-08-29 02:10:36 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,46 @@
-const { defineConfig } = require('@vue/cli-service');
-const AutoImport = require('unplugin-auto-import/webpack')
-const Components = require('unplugin-vue-components/webpack')
-const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
-module.exports = defineConfig({
-  // publicPath: '/',
-  lintOnSave: false,
-  transpileDependencies: true,
-  productionSourceMap: false,
-  configureWebpack: {
-    resolve: {
-      alias: {
-        layouts: '@/layouts',
-        assets: '@/assets',
-        components: '@/components',
-        network: '@/network',
-        views: '@/views',
-        utils: '@/utils',
-      },
-    },
-    plugins: [
-      AutoImport({
-        resolvers: [ElementPlusResolver()],
-      }),
-      Components({
-        resolvers: [ElementPlusResolver()],
-      }),
-    ],
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
   },
-});
+
+  chainWebpack: config => {
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+  },
+
+  pwa: {
+    workboxOptions: {
+      // https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
+      skipWaiting: true,
+      clientsClaim: true,
+      importWorkboxFrom: 'local',
+      importsDirectory: 'js',
+      navigateFallback: '/',
+      navigateFallbackBlacklist: [/\/api\//]
+    }
+  }
+};
