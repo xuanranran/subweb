062fe2c3af3685ae602ca792b0c6ad2943fa70b1
LovinYarn <juanshengyuan@gmail.com>
2024-08-29 01:59:48 +0800

    Revert "test"
    This reverts commit dac40b64ce4c638580c7cf44591e7aef33de0b3d.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    'vue/setup-compiler-macros': true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/prettier', //使用 prettier airbnb standard
   ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'semi': 0
-  },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'max-len': ['error', { code: 120 }],
+  },
+};
--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
-# ---- Dependencies ----
-FROM node:16-alpine AS build
-WORKDIR /app
-COPY . .
-RUN yarn install
-RUN yarn build
+FROM node:14-alpine AS build
+LABEL maintainer="Stille <stille@ioiox.com>"
 
-FROM nginx:1.24-alpine
+ENV VERSION 2.0
+
+WORKDIR /app
+COPY . /app
+RUN npm install
+RUN npm run build
+
+FROM nginx:1.16-alpine
 COPY --from=build /app/dist /usr/share/nginx/html
+COPY . /app
 EXPOSE 80
-CMD [ "nginx", "-g", "daemon off;" ]
+CMD [ "sh", "-c", "/app/start.sh" ]
